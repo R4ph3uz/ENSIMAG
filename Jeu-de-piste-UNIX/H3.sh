ccc798060813a364d0fa2c9da44b144ad0a3df26
Raphael E <raphael.euzeby@grenoble-inp.org>
2024-04-08 15:05:36 +0200

    ajout jeu de piste Unix ensimag

---
+++ Jeu-de-piste-UNIX/H3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for ((i=0;i<10000;i++))
+do
+	cp ~jdpunix/jeu-de-piste/etape-H4/$i.txt ~/Downloads/H4/
+done
---
+++ Jeu-de-piste-UNIX/d3.py
@@ -0,0 +1,200 @@
+#! /usr/bin/env python3
+# 2023-09-04 09:47:07
+from __future__ import print_function
+
+dec = str.maketrans("1234567890/abcdefghijklmnopqrstuvwxyz",
+                    "abcdefghijklmnopqrstuvwxyz/1234567890")
+print("La réponse est :")
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+print(chr(53 + 1), end='')
+
+
+
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+print(chr(50 + 2), end='')
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+print(chr(99 + 3), end='')
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+
+if 0 ==0:
+    print('', end='')
+
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+print(chr(49 + 4), end='')
+exec('96 q == z: eg9ci("qqryrqstyt")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("quwrw")'.translate(dec))
+# edf2cb38-2d40-11ed-b036-4b3a7e366466
+while False:
+    print("edf30be8-2d40-11ed-bb41-9325ce46f3d6")
+print(chr(51 + 5), end='')
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+print(chr(92 + 6), end='')
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+
+if 0 != 0:
+    print('')
+
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+print(chr(48 + 7), end='')
+
+if 0 == 0:
+    print('', end='')
+
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+print(chr(90 + 8), end='')
+
+if 0 == 0:
+    print('', end='')
+
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+print(chr(92 + 9), end='')
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+exec('96 q == z: eg9ci("qqryrqstyt")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("quwrw")'.translate(dec))
+# edf2cb38-2d40-11ed-b036-4b3a7e366466
+while False:
+    print("edf30be8-2d40-11ed-bb41-9325ce46f3d6")
+print(chr(90 + 10), end='')
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+print(chr(46 + 11), end='')
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+print(chr(42 + 12), end='')
+exec('96 q == z: eg9ci("qqryrqstyt")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("quwrw")'.translate(dec))
+# edf2cb38-2d40-11ed-b036-4b3a7e366466
+while False:
+    print("edf30be8-2d40-11ed-bb41-9325ce46f3d6")
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+print(chr(35 + 13), end='')
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+print(chr(39 + 14), end='')
+exec('96 q == z: eg9ci("qqryrqstyt")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("quwrw")'.translate(dec))
+# edf2cb38-2d40-11ed-b036-4b3a7e366466
+while False:
+    print("edf30be8-2d40-11ed-bb41-9325ce46f3d6")
+print(chr(31 + 15), end='')
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+print(chr(41 + 16), end='')
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+print(chr(32 + 17), end='')
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+print(chr(30 + 18), end='')
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+print(chr(29 + 19), end='')
+exec('96 q == z: eg9ci("qqryrqstyt")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("quwrw")'.translate(dec))
+# edf2cb38-2d40-11ed-b036-4b3a7e366466
+while False:
+    print("edf30be8-2d40-11ed-bb41-9325ce46f3d6")
+print(chr(33 + 20), end='')
+exec('96 q == z: eg9ci("qquswvsvv")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("squzq")'.translate(dec))
+# edf6012c-2d40-11ed-b2fb-d7c640c2143f
+while False:
+    print("edf63b2e-2d40-11ed-bec9-d333130f6162")
+print(chr(34 + 21), end='')
+exec('96 q == z: eg9ci("qyvrrqrq")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("rtvwv")'.translate(dec))
+# edefa34a-2d40-11ed-83c3-a771cbeca74a
+while False:
+    print("edefd78e-2d40-11ed-88c7-c734750d7061")
+exec('96 q == z: eg9ci("rvyrzrzqrr")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvuuw")'.translate(dec))
+# edf9438c-2d40-11ed-aaf8-5bac09cdcf0f
+while False:
+    print("edf97c58-2d40-11ed-899a-63144ebb2a3a")
+print(chr(29 + 22), end='')
+exec('96 q == z: eg9ci("qqqtrvqrs")'.translate(dec))
+exec('96 qrst != qrst: eg9ci("qvryz")'.translate(dec))
+# edec7544-2d40-11ed-993e-b7bdb0f3da01
+while False:
+    print("edeca91a-2d40-11ed-9efd-9fbed761556e")
+print(chr(26 + 23), end='')
+print()
